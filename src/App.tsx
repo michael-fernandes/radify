318047465c5b682d84b36727ae42912d4e9d07de
Michael Fernandes <m.fern93@gmail.com>
2022-08-18 00:12:34 -0700

    track

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import { useEffect, useState } from 'react';
 import Footer from './Components/Footer/Footer';
 import ReactGA from 'react-ga';
 
-if (process.env.REACT_APP_GA_TRACKER && process.env.NODE_ENV === "development") {
+if (process.env.REACT_APP_GA_TRACKER && process.env.NODE_ENV !== "development") {
   ReactGA.initialize('UA-73790222-1');
 }
 
