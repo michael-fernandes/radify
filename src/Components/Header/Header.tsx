0cd6e7e85ee7acc182ee2e8e775c7bae3243570c
Michael Fernandes <m.fern93@gmail.com>
2022-09-21 12:52:54 -0700

    add citations for data

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -1,12 +1,13 @@
 import styles from './Header.module.css'
-import { CONTENT } from '../../Constants/constants';
 
 const Header = () => {
   return (
     <header className={styles.header}>
       <h1 className={styles.title}>Radify</h1>
       <p className={styles.intro}>
-        {CONTENT.intro}
+        Visualize the seasonality in inflation data.
+        Data sources include Consumer Price Index(CPI) (% YoY Change) and commoditiy index from
+        the <a href="https://fred.stlouisfed.org/series/CPIAUCSL">St. Louis Fed</a> and <a href="https://www.bls.gov/charts/consumer-price-index/consumer-price-index-average-price-data.htm">Us Labor Department</a> respectively.
       </p>
     </header>
   );
